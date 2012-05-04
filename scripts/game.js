6582cebdf9e7be77a88a7d8de050118e80eb55d8
Jonathan Christian <jont.christian@gmail.com>
2012-05-03 17:29:22 -0700

    Started a completely new implementation.

--- scripts/game.js
+++ scripts/game.js
@@ -1,84 +1,102 @@
-function Game(gameBoard) {
-	var gameBoard = gameBoard;
-	var timerId;
-
-	this.play = function() {
-		timerId = setInterval(gameBoard.moveNext, 500);
-	}
-
-	this.pause = function() {
-		clearInterval(timerId);
-	}
+function Range(start, end) {
+	var array = new Array();
+	var index = 0;
+	for(var i=start; i<end; i++)
+		array[index] = i;
+	return array;
 }
 
-function GameBoard(pieceGenerator) {
-	var pieceGenerator = pieceGenerator;
-	var pieceInPlay = new EmptyPiece();
+//Input Controller
+function InputController(game) {
+	this.game = game;
+	this.functionMapping = {
+		37:game.movePieceLeft,
+		38:game.rotatePiece,
+		39:game.movePieceRight,
+		40:game.movePieceDown,
+	};
+}
+InputController.prototype.handleKeyPress = function(e) {
+	var e = window.event || e
+	var key = e.charCode || e.keyCode
 
-	this.moveNext = function() {
-		if(!isSettled(pieceInPlay)) {
-			pieceInPlay.moveDown();
-			checkForCompletedLines();
-		}
-		else {
-			pieceInPlay = pieceGenerator.pop();
-			pieceInPlay.moveToTop();			
-		}
-	}
-
-	var isSettled = function(piece) {
-		return !piece.canMoveDown(gameBoard);
-	}
-
-	var checkForCompletedLines = function() {
-		
-	}
+	if(this.functionMapping[key] != undefined)
+		this.functionMapping[key]();
+}
+InputController.prototype.play = function(window) {
+	game.play(window);
+}
+InputController.prototype.pause = function(window) {
+	game.pause(window);
 }
 
-function PieceGenerator() {
-	this.pop = function() {
-		return new LinePiece();
-	}
-}
-
+//Pieces
 function LinePiece() {
-	this.moveToTop = function() {
-		console.log("Line Piece moved to top");
-	}
 
-	this.moveDown = function() {
-		console.log("Line Piece moved down");
-	}
-
-	this.canMoveDown = function(gameBoard) {
-		return true;
-	}
+}
+LinePiece.prototype.moveLeft = function() {
+	console.log("Moved Left");
+}
+LinePiece.prototype.moveRight = function() {
+	console.log("Moved Right");
+}
+LinePiece.prototype.moveDown = function() {
+	console.log("Moved Down");
+}
+LinePiece.prototype.rotate = function() {
+	console.log("Rotated");
 }
 
-function EmptyPiece() {
-	this.canMoveDown = function(gameBoard) {
-		return false;
-	}
+//Units
+function EmptyUnit() {
+
 }
 
-var pieceGenerator = new PieceGenerator();
-var gameBoard = new GameBoard(pieceGenerator);
-var game = new Game(gameBoard);
-
-var moveNext = function() {
-	gameBoard.moveNext();
+//Board
+function Board() {
+	this.units = new Range(0, 19).map(function(x) { return new Range(0, 9).map(function(y) { return new EmptyUnit() }) });
 }
 
-var play = function() {
-	timerId = setInterval(moveNext, 500);
+//Game
+function Game(board) {
+	this.board = board;
+	this.pieceInPlay = new LinePiece();
+	this.timerId;
+}
+Game.prototype.movePieceLeft = function() {
+	this.pieceInPlay.moveLeft();
+}
+Game.prototype.movePieceRight = function() {
+	this.pieceInPlay.moveRight();
+}
+Game.prototype.movePieceDown = function() {
+	this.pieceInPlay.moveDown();
+}
+Game.prototype.rotatePiece = function() {
+	this.pieceInPlay.rotate();
+}
+Game.prototype.play = function(window) {
+	var $this = this;
+	this.timerId = window.setInterval(function() { $this.movePieceDown() }, 500);
+}
+Game.prototype.pause = function(window) {
+	window.clearInterval(this.timerId);
 }
 
-var pause = function() {
-	clearInterval(timerId);
+//BoardRenderer
+function BoardRenderer() {}
+BoardRenderer.prototype.render = function(board) {
+
 }
+
+var board = new Board();
+var game = new Game(board);
+var inputController = new InputController(game);
 
 var playButton = document.getElementById('playButton');
 var pauseButton = document.getElementById('pauseButton');
+var window = this;
 
-playButton.addEventListener('click', game.play);
-pauseButton.addEventListener('click', game.pause);
+playButton.addEventListener('click', function() { inputController.play(window) });
+pauseButton.addEventListener('click', function() { inputController.pause(window) });
+document.addEventListener('keydown', inputController.handleKeyPress);
