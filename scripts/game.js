eeda70de6bbbe08a57cb12d3d26951177edbe0b3
Jonathan Christian <jont.christian@gmail.com>
2012-05-18 10:32:51 -0700

    Added rendering of the score.

--- scripts/game.js
+++ scripts/game.js
@@ -1,35 +1,32 @@
 function Range(start, end) {
 	var array = [];
 	var index = 0;
-	for (var i=start; i<=end; i++) {
+	for (var i = start; i <= end; i++) {
 		array[index++] = i;
 	}
 	return array;
 }
 
 //Input Controller
+
+
 function InputController(game) {
 	this.game = game;
 	this.functionMapping = {
-		37:function() { game.movePieceLeft(); },
-		38:function() { game.rotatePiece(); },
-		39:function() { game.movePieceRight(); },
-		40:function() { game.movePieceDown(); }
+		37: function() { game.movePieceLeft(); },
+		38: function() { game.rotatePiece(); },
+		39: function() { game.movePieceRight(); },
+		40: function() { game.movePieceDown(); }
 	};
 }
 InputController.prototype.handleKeyPress = function(e) {
 	var args = window.event || e;
 	var key = args.charCode || args.keyCode;
 
-	if(this.functionMapping[key] !== undefined)
-		this.functionMapping[key]();
+	if (this.functionMapping[key] !== undefined) this.functionMapping[key]();
 };
-InputController.prototype.play = function(window) {
-	game.play(window);
-};
-InputController.prototype.pause = function(window) {
-	game.pause(window);
-};
+InputController.prototype.play = function(window) { game.play(window); };
+InputController.prototype.pause = function(window) { game.pause(window); };
 
 //Pieces
 function Piece(representations) {
@@ -62,25 +59,39 @@ Piece.prototype.getUnits = function() {
 	});
 };
 Piece.representations = [
-	[[new Point(0, 0), new Point(0, 1), new Point(0, 2), new Point(0, 3)],
-     [new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(3, 0)]],
- 	[[new Point(0, 1), new Point(1, 1), new Point(1, 0), new Point(2, 0)],
-	 [new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(1, 2)]],
- 	[[new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(2, 1)],
-	 [new Point(1, 0), new Point(1, 1), new Point(0, 1), new Point(0, 2)]],
-	[[new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(0, 2)],
-	 [new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(2, 1)],
-	 [new Point(0, 0), new Point(0, 1), new Point(0, 2), new Point(1, 0)],
-	 [new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(2, 1)]],
-	[[new Point(0, 0), new Point(0, 1), new Point(0, 2), new Point(1, 2)],
-	 [new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(2, 0)],
-	 [new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(1, 2)],
-	 [new Point(0, 1), new Point(0, 0), new Point(1, 0), new Point(2, 0)]],
-	[[new Point(0, 1), new Point(1, 1), new Point(1, 0), new Point(2, 1)],
-	 [new Point(0, 0), new Point(0, 1), new Point(0, 2), new Point(1, 1)],
-	 [new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(1, 1)],
-	 [new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(0, 1)]],
- 	[[new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1)]]
+	[
+		[new Point(0, 0), new Point(0, 1), new Point(0, 2), new Point(0, 3)],
+		[new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(3, 0)]
+	],
+	[
+		[new Point(0, 1), new Point(1, 1), new Point(1, 0), new Point(2, 0)],
+		[new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(1, 2)]
+	],
+	[
+		[new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(2, 1)],
+		[new Point(1, 0), new Point(1, 1), new Point(0, 1), new Point(0, 2)]
+	],
+	[
+		[new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(0, 2)],
+		[new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(2, 1)],
+		[new Point(0, 0), new Point(0, 1), new Point(0, 2), new Point(1, 0)],
+		[new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(2, 1)]
+	],
+	[
+		[new Point(0, 0), new Point(0, 1), new Point(0, 2), new Point(1, 2)],
+		[new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(2, 0)],
+		[new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(1, 2)],
+		[new Point(0, 1), new Point(0, 0), new Point(1, 0), new Point(2, 0)]
+	],
+	[
+		[new Point(0, 1), new Point(1, 1), new Point(1, 0), new Point(2, 1)],
+		[new Point(0, 0), new Point(0, 1), new Point(0, 2), new Point(1, 1)],
+		[new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(1, 1)],
+		[new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(0, 1)]
+	],
+	[
+		[new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1)]
+	]
 ];
 
 //Point
@@ -96,18 +107,22 @@ Point.prototype.equals = function(point) {
 };
 
 //Units
-function EmptyUnit() { }
-EmptyUnit.prototype.isEmpty = function() { return true; };
+function EmptyUnit() {}
+EmptyUnit.prototype.isEmpty = function() {
+	return true;
+};
 
-function LineUnit() { }
-LineUnit.prototype.isEmpty = function() { return false; };
+function LineUnit() {}
+LineUnit.prototype.isEmpty = function() {
+	return false;
+};
 
 //Board
 function Board(width, height) {
 	this.width = width;
 	this.height = height;
-	this.units = new Range(0, height-1).map(function(x) {
-		return new Range(0, width-1).map(function(y) {
+	this.units = new Range(0, height - 1).map(function(x) {
+		return new Range(0, width - 1).map(function(y) {
 			return new EmptyUnit();
 		});
 	});
@@ -126,12 +141,14 @@ Board.prototype.getUnits = function() {
 };
 Board.prototype.addPiece = function(piece) {
 	var board = this;
-	piece.getUnits().forEach(function(unit) { board.units[unit.point.y][unit.point.x] = unit; });
+	piece.getUnits().forEach(function(unit) {
+		board.units[unit.point.y][unit.point.x] = unit;
+	});
 };
 Board.prototype.clearRows = function() {
 	var rowsCleared = 0;
 	var board = this;
-	while(this.hasCompletedRow()) {
+	while (this.hasCompletedRow()) {
 		this.removeRow(this.getCompletedRow());
 		rowsCleared++;
 	}
@@ -139,28 +156,36 @@ Board.prototype.clearRows = function() {
 	return rowsCleared;
 };
 Board.prototype.hasCompletedRow = function() {
-	return this.units.some(function(line) { return line.every(function(unit) { return !unit.isEmpty(); }); });
+	return this.units.some(function(line) {
+		return line.every(function(unit) {
+			return !unit.isEmpty();
+		});
+	});
 };
 Board.prototype.getCompletedRow = function() {
-	return this.units.filter(function(line) { return line.every(function(unit) { return !unit.isEmpty(); }); })[0];
+	return this.units.filter(function(line) {
+		return line.every(function(unit) {
+			return !unit.isEmpty();
+		});
+	})[0];
 };
 Board.prototype.removeRow = function(line) {
 	var board = this;
 	var lineIndex = this.units.indexOf(line);
 
-	for(var i=lineIndex-1; i>=0; i--) {
+	for (var i = lineIndex - 1; i >= 0; i--) {
 		this.moveRowDown(this.units[i]);
 	}
 
-	for(var j=0; j<this.units[0].length; j++) {
+	for (var j = 0; j < this.units[0].length; j++) {
 		this.units[0][j] = new EmptyUnit();
 	}
 };
 Board.prototype.moveRowDown = function(line) {
 	var lineIndex = this.units.indexOf(line);
 
-	for(var i=0; i<this.units[lineIndex].length; i++) {
-		this.units[lineIndex+1][i] = this.units[lineIndex][i];
+	for (var i = 0; i < this.units[lineIndex].length; i++) {
+		this.units[lineIndex + 1][i] = this.units[lineIndex][i];
 	}
 };
 
@@ -171,7 +196,7 @@ function Game(board, hitDetector, pieceGenerator, scorekeeper) {
 	this.hitDetector = hitDetector;
 	this.pieceGenerator = pieceGenerator;
 	this.scorekeeper = scorekeeper;
-	this.move = function() { };
+	this.move = function() {};
 
 	this.playNewPiece();
 }
@@ -182,19 +207,20 @@ Game.prototype.movePieceRight = function() {
 	this.movePiece(function(piece) { piece.moveRight(); });
 };
 Game.prototype.movePieceDown = function() {
-	if(this.hitDetector.canMove(this.pieceInPlay, this.board, function(piece) { piece.moveDown(); })) {
+	if (this.hitDetector.canMove(this.pieceInPlay, this.board, function(piece) { piece.moveDown(); })) {
 		this.pieceInPlay.moveDown();
 		this.move();
-	}
-	else {
+	} else {
 		this.settleCurrentPiece();
 	}
 };
 Game.prototype.rotatePiece = function() {
-	this.movePiece(function(piece) { piece.rotate(); });
+	this.movePiece(function(piece) {
+		piece.rotate();
+	});
 };
 Game.prototype.movePiece = function(movement) {
-	if(this.hitDetector.canMove(this.pieceInPlay, this.board, movement)) {
+	if (this.hitDetector.canMove(this.pieceInPlay, this.board, movement)) {
 		movement(this.pieceInPlay);
 		this.move();
 	}
@@ -210,9 +236,9 @@ Game.prototype.playNewPiece = function() {
 	this.pieceInPlay.move(this.startingPoint);
 };
 Game.prototype.play = function(theWindow) {
-	if(this.timerId === undefined) {
+	if (this.timerId === undefined) {
 		var $this = this;
-		this.timerId = theWindow.setInterval(function() { $this.movePieceDown(); }, 500);	
+		this.timerId = theWindow.setInterval(function() { $this.movePieceDown(); }, 500);
 	}
 };
 Game.prototype.pause = function(theWindow) {
@@ -223,7 +249,7 @@ Game.prototype.pause = function(theWindow) {
 //PieceGenerator
 function PieceGenerator() {}
 PieceGenerator.prototype.pop = function() {
-	return new Piece(Piece.representations[Math.floor(Math.random()*Piece.representations.length)%Piece.representations.length]);
+	return new Piece(Piece.representations[Math.floor(Math.random() * Piece.representations.length) % Piece.representations.length]);
 };
 
 //HitDetector
@@ -232,8 +258,7 @@ HitDetector.prototype.canMove = function(piece, board, movement) {
 	var clonedPiece = jQuery.extend(true, {}, piece);
 	movement(clonedPiece);
 
-	if(!this.isInBounds(clonedPiece, board))
-		return false;
+	if (!this.isInBounds(clonedPiece, board)) return false;
 
 	return clonedPiece.getUnits().every(function(pieceUnit) {
 		return board.getUnits().every(function(boardUnit) {
@@ -251,12 +276,12 @@ function Scorekeeper() {
 	this.rowsCleared = 0;
 	this.score = 0;
 	this.scoringAlgorithm = function(numberOfRowsCleared, level) {
-		return numberOfRowsCleared !== 0 ? Math.pow(2, numberOfRowsCleared-1) * level * 97 : 0;
+		return numberOfRowsCleared !== 0 ? Math.floor(Math.pow(2, numberOfRowsCleared - 1) * Math.log(this.rowsCleared+1) * 100) : 0;
 	};
 }
 Scorekeeper.prototype.updateScoreFor = function(numberOfRowsCleared) {
 	this.rowsCleared += numberOfRowsCleared;
-	this.score += this.scoringAlgorithm(numberOfRowsCleared, this.getLevel());
+	this.score += this.scoringAlgorithm(numberOfRowsCleared, this.rowsCleared);
 };
 Scorekeeper.prototype.getLevel = function() {
 	return Math.ceil(this.rowsCleared / 10);
@@ -267,14 +292,20 @@ function GameRenderer(game, unitRenderers) {
 	this.unitRenderers = unitRenderers;
 	this.game = game;
 
-	this.game.tick = function() { this.render(); };
+	this.game.tick = function() {
+		this.render();
+	};
 }
 GameRenderer.prototype.renderItem = function(itemToRender) {
 	var $this = this;
 	itemToRender.getUnits().forEach(function(unit) {
-		var renderer = $this.unitRenderers.filter(function(renderer) { return renderer.canRender(unit); })[0];
-		if(renderer === undefined)
-			throw { name: "Missing Unit Renderer", message: "No renderer for unit [" + unit + "]" };
+		var renderer = $this.unitRenderers.filter(function(renderer) {
+			return renderer.canRender(unit);
+		})[0];
+		if (renderer === undefined) throw {
+			name: "Missing Unit Renderer",
+			message: "No renderer for unit [" + unit + "]"
+		};
 
 		renderer.render(unit);
 	});
@@ -291,10 +322,16 @@ function GenericUnitRenderer(canRender, className) {
 GenericUnitRenderer.prototype.render = function(unit) {
 	var className = "col" + (unit.point.x + 1);
 	var cell = $("#row" + (unit.point.y + 1)).find("." + className);
-	
+
 	cell.removeClass();
 	cell.addClass(className);
 	cell.addClass(this.className);
+};
+
+function ScoreRenderer() { }
+ScoreRenderer.prototype.render = function(scorekeeper) {
+    $('#Points').text(scorekeeper.score);
+    $('#Lines').text(scorekeeper.rowsCleared);
 };
 
 //Initialization
@@ -303,8 +340,10 @@ var pieceGenerator = new PieceGenerator();
 var scorekeeper = new Scorekeeper();
 var game = new Game(board, new HitDetector(), pieceGenerator, scorekeeper);
 var inputController = new InputController(game);
-var renderer = new GameRenderer(game, [new GenericUnitRenderer(function(unit) { return unit.isEmpty(); }, "empty"), new GenericUnitRenderer(function(unit) { return !unit.isEmpty(); }, "line")]);
-game.move = function() { renderer.render(); };
+var renderer = new GameRenderer(game, [new GenericUnitRenderer(function(unit) { return unit.isEmpty(); }, "empty"),
+                                       new GenericUnitRenderer(function(unit) { return !unit.isEmpty(); }, "line")]);
+var scoreRenderer = new ScoreRenderer();
+game.move = function() { renderer.render(); scoreRenderer.render(scorekeeper); };
 
 var playButton = document.getElementById('playButton');
 var pauseButton = document.getElementById('pauseButton');
